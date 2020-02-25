dfaf67faa2c26e62c3671a01ce4f94e4c8b6c056
ji-minseo <eldkfnrkz@naver.com>
2020-02-25 22:17:58 +0900

    add 06

--- practice/queue-list.java
+++ practice/queue-list.java
@@ -4,5 +4,20 @@ public class IntAryQueue {
     private int max; // 큐 용량
     private int num; // 현재 데이터 수
     private int[] que; //큐 본체
+
+    //실행 시 예외 : 큐가 비어 있음
+    public class EmptyQueueException extends RuntimeException {
+        public EmptyQueueException() {}
+    }
+
+    //실행 시 예외 : 큐가 가득 참
+    public class OverflowQueueException extends RuntimeException {
+        public OverflowQueueException() {}
+    }
+}
+
+//생성자
+public IntAryQueue(int capacity) {
+    
 }
 
