7b1b7f7b1ffe2561d46fbcd1e6f39e053dfd382d
ji-minseo <eldkfnrkz@naver.com>
2020-03-18 20:44:39 +0900

    큐 만들기

--- practice/queue-ring.java
+++ practice/queue-ring.java
@@ -3,6 +3,8 @@ package chap04;
 public class IntAryQueue {
     private int max; // 큐 용량
     private int num; // 현재 데이터 수
+    private int rear;
+    private int front;
     private int[] que; //큐 본체
 
     //실행 시 예외 : 큐가 비어 있음
@@ -18,7 +20,7 @@ public class IntAryQueue {
 
 //생성자
 public IntAryQueue(int capacity) {
-    num = 0; //처음 데이터 개수
+    num, front, rear = 0; //프론트, 레어의 첫위치
     max = capacity; //큐의 크기
     try {
         que = new int[max];
@@ -27,26 +29,24 @@ public IntAryQueue(int capacity) {
     }  
 }
 
-public int enque(int x) throws OverflowQueueException {
-    if(num >= max)//끝까지 큐가 찬 경우
+public int enque(int x) throws OverflowQueueException {//rear 로 넣기
+    if(num >= max)
         throw new OverflowQueueException();
     else {
-        que[num] = x;
+        que[rear++] = x;
         num++;
-        
+        // rear++;
+
     }
     return que;
-    //return que[num++] = x;
-
 }
 
-public int deque() throws EmptyQueueException {
+public int deque() throws EmptyQueueException {//front로 꺼내기
     if(num == 0)
         throw new EmptyQueueException();
     else {
-        for (int i = 0; i < max; i++) {
-            que[i] = que[i+1];
-        }
+        que[front++] = 0;
+        //front++;
         num--;
     }
     return que;
